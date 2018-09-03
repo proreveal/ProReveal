b300a1e5a300658c7856dcd102c10030311b3060
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2018-09-03 13:51:19 +0900

    resolve conflict

--- src/app/safeguard/safeguard.ts
+++ src/app/safeguard/safeguard.ts
@@ -2,21 +2,9 @@ import { Query } from "../data/query";
 import { Operators } from "./operator";
 import { ConstantTrait } from "./constant";
 import { ExplorationNode } from "../exploration/exploration-node";
-import { VariableTrait, SingleVariable } from "./variable";
-import { AggregateQuery } from "../data/query";
-import { NormalDistribution } from "./normal";
-
-export enum SafeguardTypes {
-    None = 0,
-    Point = 1,
-    Range = 2,
-    Comparative = 3,
-    Distributive = 4
-}
+import { VariableTrait } from "./variable";
 
 export class Safeguard {
-    static normal = new NormalDistribution();
-
     constructor(
         public variable: VariableTrait,
         public operator: Operators,
@@ -24,33 +12,5 @@ export class Safeguard {
         public node: ExplorationNode
     ) {
 
-    }
-
-    static EstimatePoint(query: AggregateQuery, variable: SingleVariable,
-        operator: Operators, constant: number) {
-        let result = query.result[variable.fieldGroupedValue.hash].accumulatedValue;
-        let ai = query.accumulator.approximate(
-                result,
-                query.progress.processedPercent(),
-                query.dataset.length);
-
-        let z = (constant - ai.mean) / ai.stdem;
-        let cp = Safeguard.normal.cdf(z);
-
-        if(operator == Operators.GreaterThan || operator == Operators.GreaterThanOrEqualTo) {
-            return 1 - cp;
-        }
-        else if(operator == Operators.LessThan || operator == Operators.LessThanOrEqualTo) {
-            return cp;
-        }
-        else {
-            throw new Error(`Invalid operator ${operator}`);
-        }
-    }
-
-    // http://195.134.76.37/applets/AppletTtest/Appl_Ttest2.html
-    static CompareMeans(query: AggregateQuery, variable: SingleVariable,
-        operator: Operators, variable2: SingleVariable) {
-        return 0.5
     }
 }
