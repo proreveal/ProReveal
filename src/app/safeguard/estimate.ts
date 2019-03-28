4d584f1453aa4ce40a1cdc960a22b7a2e208e9ce
e- <jmjo@hcil.snu.ac.kr>
2019-03-28 10:32:27 +0900

    refactor

--- src/app/safeguard/estimate.ts
+++ src/app/safeguard/estimate.ts
@@ -1,11 +1,10 @@
 import { NormalDistribution } from "./normal";
 import { AggregateQuery } from "../data/query";
-import { SingleVariable, VariablePair, VariableTrait, CombinedVariablePair } from "./variable";
+import { VariablePair, VariableTrait, CombinedVariablePair } from "./variable";
 import { Operators } from "./operator";
 import { ValueConstant, RankConstant, RangeConstant, RangeRankConstant, PowerLawConstant, NormalConstant, LinearRegressionConstant } from "./constant";
 import { isNull } from "util";
 import { Validity, PValue, Truthiness, Quality, Error } from "./validity";
-import { isFulfilled } from "q";
 import { ApproximatedInterval } from "../data/approximated-interval";
 
 const normal = new NormalDistribution();
