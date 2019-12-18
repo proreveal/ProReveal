d3de79528727bc4b0b4c1576e8774eb76699896b
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2019-12-18 16:44:39 +0900

    visibility states

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -11,7 +11,7 @@ export class SocketService {
     serverInfo: any;
     url = Env.apiHost
 
-    constructor(private storage:StorageService) {
+    constructor(private storage: StorageService) {
         let ws = io(Env.apiHost, { transports: ['websocket'] })
         this.ws = ws;
 
@@ -28,6 +28,44 @@ export class SocketService {
         });
 
         (window as any).ss = this;
+
+        this.setVisibilityUpdate();
+    }
+
+    setVisibilityUpdate() {
+        let hidden:string, visibilityChange:string;
+        let doc = document as any;
+        if (typeof doc.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+            hidden = "hidden";
+            visibilityChange = "visibilitychange";
+        } else if (typeof doc.msHidden !== "undefined") {
+            hidden = "msHidden";
+            visibilityChange = "msvisibilitychange";
+        } else if (typeof doc.webkitHidden !== "undefined") {
+            hidden = "webkitHidden";
+            visibilityChange = "webkitvisibilitychange";
+        }
+
+        if (typeof document.addEventListener === "undefined" || hidden === undefined) {
+            console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+        } else {
+            // Handle page visibility change
+            document.addEventListener(visibilityChange, () => {
+                if (document[hidden]) {
+                    this.emitSessionHidden();
+                } else {
+                    this.emitSessionVisible();
+                }
+            }, false);
+        }
+    }
+
+    emitSessionHidden() {
+        this.ws.emit('REQ/session/hidden')
+    }
+
+    emitSessionVisible() {
+        this.ws.emit('REQ/session/visible');
     }
 
     disconnect() {
@@ -37,4 +75,6 @@ export class SocketService {
     kill() {
         this.ws.emit('kill');
     }
+
+
 }
